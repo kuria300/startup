6c9ee6fbdf9339af9491f2d0937bd0f8d33d9bfc
EugeneK <139058952+kuria300@users.noreply.github.com>
2026-09-04 17:18:59 +0300

    Update validation.ts

--- lib/validation.ts
+++ lib/validation.ts
@@ -5,19 +5,7 @@ export const userSchema= z.object({
    title: z.string().min(3).max(100),
    description: z.string().min(20),
    category: z.string().min(3).max(20),
-   link :z.string().url().refine(async (url)=>{
-      try{
-         // Perform a HEAD request to the URL to get headers only (more efficient than GET)
-        const res= await fetch(url, {method: "HEAD"});
-         //extracts content-type from headers
-         const content= res.headers.get("content-type");
-
-         return content?. startsWith("image/")
-
-      }catch{
-         return false
-      }
-   }),
+   link: z.string().url(),
    pitch: z.string().min(10),
 
 })
